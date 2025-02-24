e4e811364ff50e5db1b99d5c61f5be4f59e5cdf7
Sakshi123-tech <sakshiagnihotri292@gmail.com>
2025-02-24 20:12:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-//complete this code
-class Person {}
+// 1. Person Class
+class Person {
+  private _name: string;
+  private _age: number;
 
-class Student extends Person {}
+  // Constructor to initialize the person with name and age
+  constructor(name: string, age: number) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Teacher extends Person {}
+  // Getter for the name
+  get name(): string {
+    return this._name;
+  }
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+  // Setter for the age
+  set age(age: number) {
+    this._age = age;
+  }
+
+  // Getter for age (if you need it)
+  get age(): number {
+    return this._age;
+  }
+}
+
+// 2. Student Class that inherits from Person
+class Student extends Person {
+  // Method that logs a message indicating the student is studying
+  study(): void {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+// 3. Teacher Class that inherits from Person
+class Teacher extends Person {
+  // Method that logs a message indicating the teacher is teaching
+  teach(): void {
+    console.log(`${this.name} is teaching`);
+  }
+}
